d8ed1228a785d4848adf0a7d078085c6068cc08e
maher <maherramadan2012@hotmail.com>
2023-02-25 22:48:16 -0600

    Fix errors in css style

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 
-
 const HtmlWebpackPlugin = require.resolve('html-webpack-plugin');
 
 module.exports = {
@@ -33,41 +32,4 @@ module.exports = {
       },
     ],
   },
-=======
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = { 
-mode: 'development',
-entry: './src/index.js',
-devServer: {
-
-    static: './dist',
-
-  },
-  output: { 
-     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-
-    rules: [
-
-      {
-
-        test: /\.css$/i,
-
-        use: ['style-loader', 'css-loader'],
-
-      },
-
-    ],
-
-  },
-  plugins: [
-
-    new HtmlWebpackPlugin({ 
-        template: './src/index.html'
-    }),
-  ],
-
 };
