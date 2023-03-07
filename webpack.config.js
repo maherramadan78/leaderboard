e19cfef4120e1c8e13b98cfb58e913dfce6fb66e
maher <maherramadan2012@hotmail.com>
2023-03-07 13:50:12 -0600

    Update index.html and css files

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,28 @@
 const path = require('path');
-
-const HtmlWebpackPlugin = require.resolve('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
+  mode: 'production',
+  cache: true,
   output: {
-    filename: 'bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   devServer: {
     static: './dist',
-    hot: true,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-      template: './src/index.html',
-    }),
-  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        use: ['file-loader'],
-      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
